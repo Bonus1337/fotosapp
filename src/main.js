d2b1ef9acdc422bdb2e8d4fb6e0d7aa13f97b430
Maciej <szymanski.maciej1337@gmail.com>
2023-03-27 16:31:33 +0200

    next commit

--- src/main.js
+++ src/main.js
@@ -7,4 +7,6 @@ import 'primevue/resources/primevue.min.css'
 import 'primeicons/primeicons.css'
 import 'primeflex/primeflex.min.css'
 import 'animate.css/animate.min.css'
-createApp(App).use(store).use(router).mount('#app')
+import PrimeVue from 'primevue/config'
+
+createApp(App).use(store).use(router).use(PrimeVue).mount('#app')
